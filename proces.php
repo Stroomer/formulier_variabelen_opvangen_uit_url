2e0a6064347d3a77550f369a373013379f5b4b82
Lukas Stroomer <lukasstroomer@gmail.com>
2022-11-04 10:58:47 +0100

    first commit

---
+++ proces.php
@@ -0,0 +1,14 @@
+<?php 
+    
+    echo print_r($_GET);
+    
+    // $voornaam   = $_GET['voornaam'];
+    // $achternaam = $_GET['achternaam'];
+    // $land       = $_GET['land'];
+
+    // echo "Het formulier is verstuurd, PHP heeft de volgende waarden opgevangen!" . "<br><br>";
+
+    // echo "Voornaam:   " . $voornaam    . "<br>";
+    // echo "Achternaam: " . $achternaam  . "<br>";
+    // echo "Land:       " . $land        . "<br>";
+?>
